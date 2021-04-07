893858fc75021cad1a67e90a651e304e2a586437
Ruchir Raturi <rraturi@Ruchirs-MBP-2.fios-router.home>
2021-04-07 15:38:06 -0400

    trying this again

--- src/index.js
+++ src/index.js
@@ -4,17 +4,13 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-ReactDom.render(
-	<h1>Hello, world!</h1>, 
-	document.getElementById('root')
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
